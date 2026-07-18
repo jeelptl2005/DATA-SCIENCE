2ce069f47f4f6e38207461ec8f0e0186bfc96e8e
JEEL PATEL <s22cpjeel107@gmail.com>
2026-07-18 17:05:57 +0530

    Update main.py

--- HousePred/backend/main.py
+++ HousePred/backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="House Price Prediction API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://jp-housepredictor.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
